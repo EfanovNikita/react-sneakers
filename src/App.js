225aad0ddde7334251936db8ba1ba12132393534
Efanov Nikita <efanov.nickitka@yandex.ru>
2021-12-24 23:34:01 +0700

    create cart block

--- src/App.js
+++ src/App.js
@@ -3,6 +3,72 @@ import React from 'react';
 function App() {
   return (
     <div className="wrapper">
+      <div className='overlay'>
+        <div className='drawer'>
+          <h2>Корзина <img className='' src='/img/btn-remove.svg' alt='remove' /></h2>
+
+          <div className='items'>
+            <div className='cartItem'>
+              <div style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }} className='cartItemImg'></div>
+              <div>
+                <p>Мужские кроссовки Nike Blazer Mid Suede</p>
+                <b>10 565 руб.</b>
+              </div>
+              <img className='btnRemove' src='/img/btn-remove.svg' alt='remove' />
+            </div>
+            <div className='cartItem'>
+              <div style={{ backgroundImage: 'url(/img/sneakers/2.jpg)' }} className='cartItemImg'></div>
+              <div>
+                <p>Мужские кроссовки Nike Blazer Mid Suede</p>
+                <b>10 565 руб.</b>
+              </div>
+              <img className='btnRemove' src='/img/btn-remove.svg' alt='remove' />
+            </div>
+            <div className='cartItem'>
+              <div style={{ backgroundImage: 'url(/img/sneakers/2.jpg)' }} className='cartItemImg'></div>
+              <div>
+                <p>Мужские кроссовки Nike Blazer Mid Suede</p>
+                <b>10 565 руб.</b>
+              </div>
+              <img className='btnRemove' src='/img/btn-remove.svg' alt='remove' />
+            </div>
+            <div className='cartItem'>
+              <div style={{ backgroundImage: 'url(/img/sneakers/2.jpg)' }} className='cartItemImg'></div>
+              <div>
+                <p>Мужские кроссовки Nike Blazer Mid Suede</p>
+                <b>10 565 руб.</b>
+              </div>
+              <img className='btnRemove' src='/img/btn-remove.svg' alt='remove' />
+            </div>
+            <div className='cartItem'>
+              <div style={{ backgroundImage: 'url(/img/sneakers/2.jpg)' }} className='cartItemImg'></div>
+              <div>
+                <p>Мужские кроссовки Nike Blazer Mid Suede</p>
+                <b>10 565 руб.</b>
+              </div>
+              <img className='btnRemove' src='/img/btn-remove.svg' alt='remove' />
+            </div>
+          </div>
+
+          <div className="cartTotalBlock">
+            <ul>
+              <li>
+                <span>Итого: </span>
+                <div></div>
+                <b>21 000 руб. </b>
+              </li>
+              <li>
+                <span>Налог 5%: </span>
+                <div></div>
+                <b>1074 руб. </b>
+              </li>
+            </ul>
+            <button className='greenButton'>Оформить заказ <img src="/img/arrow.svg" alt="arrow" /></button>
+          </div>
+        </div>
+      </div>
+
+
       <header>
         <div className='headerLeft'>
           <img width={40} height={40} src='/img/logo.png' alt="logo" />
@@ -22,9 +88,20 @@ function App() {
         </ul>
       </header>
       <div className='content'>
-        <h1>Все кроссовки</h1>
+        <div className='contentHeader'>
+          <h1>Все кроссовки</h1>
+          <div className='searchBlock'>
+            <img src='/img/search.svg' alt='search' />
+            <input placeholder='Поиск...' />
+          </div>
+        </div>
+
+
         <div className='sneakers'>
           <div className='card'>
+            <div className='favorite'>
+              <img src='/img/unliked.svg' alt='unliked' />
+            </div>
             <img width={133} height={112} src='/img/sneakers/1.jpg' alt='sneakers1' />
             <h5>Мужские кроссовки Nike Blazer Mid Suede</h5>
             <div className='cardInfo'>
