704f39b0982e3ab915d7ae3378762a30a25240c6
Efanov Nikita <efanov.nickitka@yandex.ru>
2022-01-06 18:37:21 +0700

    upgrade cart component

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -6,6 +6,19 @@ function Drawer({ closeCart, items, onRemove }) {
             <div className={style.drawer}>
                 <h2>Корзина <img className='' onClick={closeCart} src='/img/btn-remove.svg' alt='close' /></h2>
 
+                {
+                    !items[0] &&
+                    <div className={style.cartEmpty}>
+                        <img className={style.cartImg} src="/img/empty-cart.jpg" alt="empty-cart" />
+                        <h2>Корзина пустая</h2>
+                        <p>Добавьте товар в корзину</p>
+                        <button className={style.greenButton} onClick={closeCart}>
+                            Вернуться назад
+                            <img className={style.leftArrow} src="/img/arrow.svg" alt="arrow" />
+                        </button>
+                    </div>
+                }
+
                 <div className={style.items}>
                     {items.map(obj => (
                         <div className={style.cartItem} key={obj.url}>
@@ -19,21 +32,23 @@ function Drawer({ closeCart, items, onRemove }) {
                     ))}
                 </div>
 
-                <div className={style.cartTotalBlock}>
-                    <ul>
-                        <li>
-                            <span>Итого: </span>
-                            <div></div>
-                            <b>21 000 руб. </b>
-                        </li>
-                        <li>
-                            <span>Налог 5%: </span>
-                            <div></div>
-                            <b>1074 руб. </b>
-                        </li>
-                    </ul>
-                    <button className={style.greenButton}>Оформить заказ <img src="/img/arrow.svg" alt="arrow" /></button>
-                </div>
+                {items[0] &&
+                    <div className={style.cartTotalBlock}>
+                        <ul>
+                            <li>
+                                <span>Итого: </span>
+                                <div></div>
+                                <b>21 000 руб. </b>
+                            </li>
+                            <li>
+                                <span>Налог 5%: </span>
+                                <div></div>
+                                <b>1074 руб. </b>
+                            </li>
+                        </ul>
+                        <button className={style.greenButton}>Оформить заказ <img src="/img/arrow.svg" alt="arrow" /></button>
+                    </div>
+                }
             </div>
         </div>
     )
